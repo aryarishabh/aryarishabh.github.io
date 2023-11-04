08d176adc85e84fe1667d3ad1f1dd13bb8ec09c3
Rishabh Arya <caffienet0code@gmail.com>
2023-11-04 14:40:25 +0530

    changes website

---
+++ assets/js/help.js
@@ -0,0 +1,13 @@
+const thisForm = document.getElementById('mentor');
+thisForm.addEventListener('submit', async function (e) {
+    e.preventDefault();
+    const formData = new FormData(thisForm).entries()
+    const response = await fetch('https://prepclubs.com/api/mentors', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(Object.fromEntries(formData))
+    });
+
+    const result = await response.json();
+    console.log(result)
+});
